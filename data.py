a2701590625b05fc6df5cc37bae47364b7521806
Marina Cottrell <marinacottrell@gmail.com>
2018-05-16 13:36:18 -0700

    one piece test

--- data.py
+++ data.py
@@ -1,1 +1,17 @@
-import music21
+from music21 import *
+
+
+score = converter.parse("fugueData/goodFugues/fugue 4 [unknown].mid")
+
+parts = [p for p in song.getElementsByClass(stream.Part)]
+
+values = []
+for p in parts:
+    p.transferOffsetToElements()
+    for m in p:
+        time = m.offset - int(m.offset)
+        if time != 0:
+            values.append(time)
+
+step = min(values)
+
