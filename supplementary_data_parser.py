b44a764accff03065f7874afc211b516ad898140
ssilverst <35214996+ssilverst@users.noreply.github.com>
2018-05-31 16:37:05 -0700

    Update supplementary_data_parser.py

--- supplementary_data_parser.py
+++ supplementary_data_parser.py
@@ -31,21 +31,41 @@ def make_one_hot_vector_X(X, m, Tx, pitch_values):
 
 def make_one_hot_vector_Y(Y, m, Ty, n_c_Y, pitch_values):
 
-	Yoh = np.zeros((m, Ty, len(pitch_values), n_c_Y-1))
-	Y = Y[:,:,:-1]
+	Yoh = np.zeros((m, Ty, n_c_Y-1, len(pitch_values)))
+	Y = Y[:][:][0:n_c_Y-1]
 	for i in range(m):
 		for j in range(Ty):
-			all_one_hots = np.zeros((len(pitch_values), n_c_Y-1))
+			all_one_hots = np.zeros((n_c_Y-1, len(pitch_values)))
 			for k in range(n_c_Y-1):
 				note = Y[i][j][k]
 				ind = pitch_values.index(note)
 				one_hot = np.zeros((len(pitch_values)))
 				one_hot[ind] = 1
-				all_one_hots[:,k] = one_hot
+				all_one_hots[k,:] = one_hot
 			Yoh[i][j] = all_one_hots
 	return Yoh
 
+def make_X_numpy_array(X):
+	m = len(X)
+	Tx = len(X[0])
+	X_np = np.zeros((m, Tx))
+	pitch_values = populate_pitch_values()
+	for i in range(m):
+		for j in range(Tx):
+			X_np[i][j] = pitch_values.index(X[i][j])
+	return X_np
 
+def make_Y_numpy_array(Y):
+	m = len(Y)
+	Ty = len(Y[0])
+	n_c_Y = len(Y[0][0])
+	Y_np = np.zeros((m, Ty, n_c_Y-1))
+	pitch_values = populate_pitch_values()
+	for i in range(m):
+		for j in range(Ty):
+			for k in range(n_c_Y-1):
+				Y_np[i][j][k] = pitch_values.index(Y[i][j][k])
+	return Y_np
 
 
 
